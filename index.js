f3d1f6fa6deefa557f78a52a2f24f777f99be96f
hustlerZzZ <102817420+hustlerZzZ@users.noreply.github.com>
2022-11-03 22:48:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ for (var i = 0; i < numberOfButtons; i++) {
 
         makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
+
     });
 }
 //Detecting Keyboard press
 document.addEventListener("keydown", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -50,4 +53,12 @@ function makeSound(key) {
             break;
 
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100)
 }
